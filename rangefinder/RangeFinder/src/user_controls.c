ad83d67ad04775d3975d47dd4fb84616c3fca287
zijun7 <159499326+zijun7@users.noreply.github.com>
2024-05-08 01:03:57 -0500

    Update user_controls.c

--- rangefinder/RangeFinder/src/user_controls.c
+++ rangefinder/RangeFinder/src/user_controls.c
@@ -29,15 +29,17 @@
 operation_mode operationMode;
 bool volatile Ping;
 static char buffer[17] = {0};
-unsigned int thresholdInput(void);
-extern unsigned threshold_range = 100; 
+int thresholdInput(int threshold_range);
+extern unsigned threshold_range = 400; 
+static bool buttonPressed = false;
 
 void initialize_controls(void) {
     Ping = false;
+    pulse_requested = false;
 }
 
 void manage_controls(void) {
-    static operation_mode pMode = -1;
+    //static operation_mode pMode = -1;
 
     if (cowpi_debounce_byte(cowpi_left_switch_is_in_left_position(), LEFT_SWITCH_LEFT) && cowpi_debounce_byte(cowpi_right_switch_is_in_left_position(),RIGHT_SWITCH_LEFT)){
         operationMode = NORMAL_OPERATION;
@@ -49,78 +51,67 @@ void manage_controls(void) {
         operationMode = CONTINUOUS_TONE;
     }
 
-
-    static bool button_is_pressed = false;
-    switch (operationMode)
-    {
-        case SINGLE_PULSE_OPERATION:
-            ;
-            bool new_button_position;
-            if ((new_button_position = cowpi_debounce_byte(cowpi_left_button_is_pressed(), LEFT_BUTTON_DOWN)) != button_is_pressed){
-                button_is_pressed = new_button_position;
-                if (button_is_pressed && operationMode == SINGLE_PULSE_OPERATION){
-                    Ping = true;
-                    pulse_requested = true;
-                }
+    if (operationMode == SINGLE_PULSE_OPERATION) {
+        bool buttonState = cowpi_debounce_byte(cowpi_left_button_is_pressed(), LEFT_BUTTON_DOWN);
+        if (buttonState != buttonPressed) {
+            buttonPressed = buttonState;
+            if (buttonPressed) {
+                pulse_requested = true;
             }
-            break;
-        case THRESHOLD_ADJUSTMENT:
-            if (operationMode != pMode){
-                threshold_range = thresholdInput();
-            }
-            Ping = false;
-            pulse_requested = false;
-            break;
-        case CONTINUOUS_TONE:
-            break;
-        case NORMAL_OPERATION:
-            break;
-        default:
-            break;
+        }
+    } else if (operationMode == THRESHOLD_ADJUSTMENT) {
+            threshold_range = thresholdInput(threshold_range);
+        Ping = false;
+        pulse_requested = false;
     }
 
-    if (operationMode != pMode){
+    
+    sprintf(buffer, "Range: %d", threshold_range);
+    display_string(4, buffer);
+    /*if (operationMode != pMode){
         pMode = operationMode;
-        /*sprintf(buffer, "%d  %d", operationMode, pMode);
+        sprintf(buffer, "%d  %d", operationMode, pMode);
         display_string(3,buffer);
         sprintf(buffer, "%d", threshold_range);
-        display_string(4, buffer);*/
-    }
+        display_string(4, buffer);
+    }*/
 }
 
-    unsigned int thresholdInput(void){
-        buffer[0] = '\0';
-        display_string(2, buffer);
-        unsigned int range;
-        do{
-            sprintf(buffer,"Input threshold");
-            display_string(0, buffer);
-            range = 0;
-            char c;
+int thresholdInput(int threshold_range){
+
+    int a = threshold_range;
+    unsigned int threshold;
+    sprintf(buffer,"Input threshold");
+    display_string(0, buffer);
+    sprintf (buffer, "%2d<= input <=%3d", 50, 400);
+    display_string(1, buffer);
+    char input;
+         do{
+            threshold = 0;
             do{
                 refresh_display();
-                while((c = cowpi_debounce_byte(cowpi_get_keypress(), KEYPAD)) != '\0') {};
+                while((input = cowpi_debounce_byte(cowpi_get_keypress(), KEYPAD)) != '\0') {};
                 do{
-                    c = cowpi_debounce_byte(cowpi_get_keypress(), KEYPAD);
                     if (!cowpi_debounce_byte(cowpi_left_switch_is_in_right_position(), LEFT_SWITCH_RIGHT) ||!cowpi_debounce_byte(cowpi_right_switch_is_in_right_position(), RIGHT_SWITCH_RIGHT)) {
-                        return range;
+                        return a;
                     }
-                }while(!(c == '#' || (c >= '0' && c <= '9')));
-                if (c >= '0' && c <= '9'){
-                    range = 10 * range + (c - '0');
-                    if (range < 1000){
-                        sprintf(buffer, "Range:     %3ucm", range);
-                        display_string(1,buffer);
-                    }
-                    while ((c = cowpi_debounce_byte(cowpi_get_keypress(), KEYPAD)) != '\0') {};
+                    input = cowpi_debounce_byte(cowpi_get_keypress(), KEYPAD);
+                }while(!(input == '#' || (input >= '0' && input <= '9')));
+                if (input >= '0' && input <= '9' && (10 * threshold + (input - '0')) < 1000){
+                    threshold = 10 * threshold + (input - '0');
+                    sprintf(buffer, "Input     %3dcm", threshold);
+                    display_string(2,buffer);
                 }
-            }while (c != '#');
-            if ( range < 50 || range > 400){
-                sprintf (buffer, "%2d < rng < %3d", 49, 401);
-                display_string(0,buffer);
+            }while (input != '#');
+            if ( threshold < 50 || threshold > 400){
+                sprintf (buffer, "Invalid input");
+                display_string(2, buffer);
             }
-        }while (range < 50 || range > 400);
-        sprintf(buffer,"Threshold  %3ucm", range);
-        display_string(1, buffer);
-        return range;
-    }
+        }while (threshold < 50 || threshold > 400);
+        sprintf(buffer,"Threshold  %3dcm", threshold);
+        display_string(2, buffer);
+        
+    a = threshold;
+    return threshold;
+
+}
